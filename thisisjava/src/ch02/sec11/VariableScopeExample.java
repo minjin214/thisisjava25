f2a2ce865c13784896ba7bd3db66ce61cf805809
minjin214 <leeminjin0214@gmail.com>
2025-09-17 14:18:10 +0900

    thisisjava

---
+++ thisisjava/src/ch02/sec11/VariableScopeExample.java
@@ -0,0 +1,19 @@
+package ch02.sec11;
+
+public class VariableScopeExample {
+	public static void main(String[] args) {
+		int v1=15;
+		int v2 = v1 - 10;
+		int v3=0;
+		/*
+		if(v1>10) {
+			int v2 = v1 - 10;
+		}
+		*/
+		v3 = v1 + v2 + 5;
+		
+		System.out.println(v3);
+		
+	}
+
+}
